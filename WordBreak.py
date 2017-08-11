4fd30b5d0883dc68f5d72a4ec4ae0587a754012b
apurva-jakhanwal <zenith.apurva143@gmail.com>
2017-08-12 00:22:53 +0530

    Word Break Initial Commit

---
+++ WordBreak.py
@@ -0,0 +1,28 @@
+'''
+Word Break
+Given an input string and a dictionary of words,
+find out if the input string can be segmented into a space-separated sequence of dictionary words.
+For example, consider the following dictionary:
+{ pear, salmon, foot, prints, footprints, leave, you, sun, girl, enjoy },
+Examples:Given the string “youenjoy”, 
+Output: True (The string can be segmented as “you enjoy”)
+Input: “youleavefootprints”,
+Output: True (The string can be segmented as “you leave footprints” or “you leave foot prints”)
+Input:salmonenjoyapples
+Output: False
+'''
+class Solution():
+    def wordBreak(self, s, dictionary):
+        if s == "" or dictionary == {}:
+            return False
+        s = s.lower()
+        for word in dictionary:
+            word = word.lower()
+            if word in s:
+                s = s.replace(word,"")
+            if s == "":
+                return True
+        return s == ""
+        
+if __name__ == '__main__':
+    obj = Solution()
---
+++ testingWordBreak.py
@@ -0,0 +1,53 @@
+import unittest
+from WordBreak import Solution
+
+class TestCases(unittest.TestCase):
+    def test_emptyString(self):
+        '''Testing for empty string'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("", {"summer"}), False)
+
+    def test_emptyDictionary(self):
+        '''Testing for empty dictionary'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("summernow", {}), False)
+
+    def test_oneWordSequence(self):
+        '''Testing one word sequence'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("hello", {"hello", "world"}), True)
+
+    def test_twoWordsSequence(self):
+        '''Testing two words sequence'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("youenjoy", {"pear", "salmon", "foot", "prints", "footprints", "leave", "you", "sun", "girl", "enjoy"}), True)
+
+    def test_moreThan2WordsSequence(self):
+        '''Testing more than two words sequence'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("youleavefootprints", {"pear", "salmon", "foot", "prints", "footprints", "leave", "you", "sun", "girl", "enjoy"}), True)
+
+    def test_unsucessfulSequence(self):
+        '''Testing unsuccessful sequence'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("salmonenjoyapples", {"pear", "salmon", "foot", "prints", "footprints", "leave", "you", "sun", "girl", "enjoy"}), False)
+
+    def test_noWordInDictionary(self):
+        '''Testing no word in dictionary'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("mycode", {"today"}), False)
+
+    def test_upperCase(self):
+        '''Testing upper case in string or words of dictionary'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("Hello", {"hello", "world"}), True)
+        self.assertEqual(obj.wordBreak("helloworld", {"Hello", "World"}), True)
+
+    def test_randomCharacters(self):
+        '''Testing random characters in string'''
+        obj = Solution()
+        self.assertEqual(obj.wordBreak("Hello12345$%^", {"hello", "world"}), False)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestCases)
+    unittest.TextTestRunner(verbosity=2).run(suite)
